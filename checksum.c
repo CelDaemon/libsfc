9a530bcd6d473ce40e7400b09f4b94a5f4ed43d2
CelDaemon <celdaemon@voidgroup.net>
2025-05-16 10:59:24 +0200

    Optimize log2z and pow2z

--- checksum.c
+++ checksum.c
@@ -1,18 +1,15 @@
+#include <strings.h>
+
 #include "mapping.h"
 
 #include "sfc.h"
 
 
-static size_t log2z(size_t x) {
-    size_t val = 0;
-    while (x >>= 1) val++;
-    return val;
+static size_t log2z(const size_t x) {
+    return ffsl((long) x);
 }
 static size_t pow2z(const size_t n) {
-    size_t c = 2;
-    for (size_t i = 0; i < n - 1; i++)
-        c *= 2;
-    return c;
+    return 1 << n;
 }
 
 uint16_t sfc_checksum(const struct sfc_rom *rom) {
@@ -26,8 +23,10 @@ uint16_t sfc_checksum(const struct sfc_rom *rom) {
         main_accumulator += data[i];
     }
     uint16_t remainder_accumulator = 0;
-    for (auto i = 0; i < main_size; i++) {
-        remainder_accumulator += data[(i % remainder_size) + main_size];
+    if (remainder_size != 0) {
+        for (auto i = 0; i < main_size; i++) {
+            remainder_accumulator += data[(i % remainder_size) + main_size];
+        }
     }
     return main_accumulator + remainder_accumulator;
 }
